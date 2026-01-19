13dc4e31e48da985b55ed01b86f93a4af2fda706
AmitElad10 <amitelad2010@gmail.com>
2026-01-19 09:43:12 +0200

    finish the project

--- app/src/main/java/com/example/ex0607/MainActivity.java
+++ app/src/main/java/com/example/ex0607/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex0607;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton btn;
+    private ImageView iVmain;
+    double x;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btn = findViewById(R.id.btn);
+        iVmain = findViewById(R.id.iVmain);
+
+    }
+
+    public void clicked(View view) {
+        x = (int)(Math.random()*4 +1);
+        if(x==1){
+            btn.setImageResource(R.drawable.num1);
+            iVmain.setImageResource(R.drawable.dog1);
+        } else if (x==2) {
+            btn.setImageResource(R.drawable.num2);
+            iVmain.setImageResource(R.drawable.dog2);
+        }
+        else{
+            btn.setImageResource(R.drawable.num3);
+            iVmain.setImageResource(R.drawable.dog3);
+        }
     }
 }
